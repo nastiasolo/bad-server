e5e570ca3cf48e173fb498f7741c85060b21b7ab
nastiasolo <ag.solodova@gmail.com>
2025-06-06 15:15:45 +0200

    app.ts fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,19 +3,28 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
+import limiter from './middlewares/rate-limit'
 import mongoose from 'mongoose'
 import path from 'path'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
+import ExpressMongoSanitize from 'express-mongo-sanitize'
 
 const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(cookieParser())
 
-app.use(cors())
+app.use(limiter)
+
+app.use(
+    cors({
+        origin: ['http://localhost', 'http://localhost:5173'], // Разрешаем оба домена
+        credentials: true,
+    })
+)
 // app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,6 +35,7 @@ app.use(urlencoded({ extended: true, limit: '10mb' }))
 app.use(json())
 
 app.options('*', cors())
+app.use(ExpressMongoSanitize())
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
